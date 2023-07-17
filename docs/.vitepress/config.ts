52da4f27ce86e35f41aeb5e75b0f7f5cbca147cb
NanakoOfficial <712111161@qq.com>
2023-07-18 01:39:28 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -60,28 +60,27 @@ export default defineConfig({
     },
 
     search: {
-    provider: 'local',
-    options: {
-      locales: {
-        zh: {
-          translations: {
-            button: {
-              buttonText: '搜索',
-              buttonAriaLabel: '搜索'
-            },
-            modal: {
-              noResultsText: '无法找到相关结果',
-              resetButtonTitle: '清除查询条件',
-              footer: {
-                selectText: '选择',
-                navigateText: '切换'
+      provider: 'local',
+      options: {
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: '搜索',
+                buttonAriaLabel: '搜索'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换'
+                }
               }
             }
           }
-        }
-      }     
+        }     
+      }
     }
-  },
-
   }
 })
