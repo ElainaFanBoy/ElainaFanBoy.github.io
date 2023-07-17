af13aeaa229fd9481cd69babd5ed4da36128c26f
NanakoOfficial <712111161@qq.com>
2023-07-18 01:30:59 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,6 +6,29 @@ import algolia from './algolia'
 export default defineConfig({
   title: "IRONY",
   description: "",
+  search: {
+    provider: 'local',
+    options: {
+      locales: {
+        zh: {
+          translations: {
+            button: {
+              buttonText: '搜索',
+              buttonAriaLabel: '搜索'
+            },
+            modal: {
+              noResultsText: '无法找到相关结果',
+              resetButtonTitle: '清除查询条件',
+              footer: {
+                selectText: '选择',
+                navigateText: '切换'
+              }
+            }
+          }
+        }
+      }     
+    }
+  },
   locales: {
     root: { label: '简体中文', lang: 'zh-CN' },
   },
