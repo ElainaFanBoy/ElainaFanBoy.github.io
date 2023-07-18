1a33c71ec246409e0ba887bbfc1a597ed070b7f2
NanakoOfficial <712111161@qq.com>
2023-07-18 09:01:13 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,32 +5,10 @@ import socialLinks from './sociallinks'
 export default defineConfig({
   title: "IRONY",
   description: "",
-  locales: {
-    root: { label: '简体中文', lang: 'zh-CN' },
-  },
   themeConfig: {
+    i18nRouting: false,
     search: {
-      provider: 'local',
-      options: {
-        locales: {
-          zh: {
-            translations: {
-              button: {
-                buttonText: '搜索',
-                buttonAriaLabel: '搜索'
-              },
-              modal: {
-                noResultsText: '无法找到相关结果',
-                resetButtonTitle: '清除查询条件',
-                footer: {
-                  selectText: '选择',
-                  navigateText: '切换'
-                }
-              }
-            }
-          }
-        }     
-      }
+      provider: 'local'
     },
     // https://vitepress.dev/reference/default-theme-config
     logo: 'https://raw.githubusercontent.com/ElainaFanBoy/ElainaFanBoy.github.io/main/docs/logo.png',
